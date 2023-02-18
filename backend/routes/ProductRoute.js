9815d059c114c866599f63e06c3375f2f6b6719c
anil843324 <anilkrsingh14200@gmail.com>
2023-02-18 13:30:34 +0530

    product Review don

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -5,6 +5,9 @@ const {
     updateProduct,
     deleteProduct,
     getSingleProduct,
+    createProductReview,
+    getSingleProductReviews,
+    deleteReview,
 } = require('../controller/ProductController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
@@ -12,7 +15,9 @@ const router = express.Router();
 
 router.route('/products').get(getAllProducts);
 
-router.route('/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), createProduct);
+router
+    .route('/product/new')
+    .post(isAuthenticatedUser, authorizeRoles('admin'), createProduct);
 
 router
     .route('/product/:id')
@@ -20,4 +25,9 @@ router
     .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct)
     .get(getSingleProduct);
 
+router.route('/product/review').post(isAuthenticatedUser, createProductReview);
+router
+    .route('/reviews')
+    .get(getSingleProductReviews)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteReview);
 module.exports = router;
